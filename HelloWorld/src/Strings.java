b0ea95c02b4dfe6e42b0d0aea2c9b1d29aa114d0
karpov2713 <karpov2713@gmail.com>
2025-02-03 18:51:32 +0300

    Повторил String
    Закончил повторение типов данных. Перехожу к повторению Циклов

---
+++ HelloWorld/src/Strings.java
@@ -0,0 +1,16 @@
+public class Strings {
+
+    public static void main(String[] args) {
+//        String s = "Hello";
+//        String w =  "world";
+//        String result = s + " " + w;
+//        System.out.println(result);
+//        System.out.println(s + " " + w);
+
+        String john = "John";
+        int age = 37;
+        System.out.println("Hello, \"" + john +"\"! \nYou are " + age + " years.");
+
+
+    }
+}
