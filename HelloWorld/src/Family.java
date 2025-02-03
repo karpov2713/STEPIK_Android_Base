2e3759d9ae236578e108b3236782e360b6f547db
karpov2713 <karpov2713@gmail.com>
2025-02-03 17:09:19 +0300

    Повторение
    Заново начал, чтобы посмотреть как GitHub файлы обновляет

---
+++ HelloWorld/src/Family.java
@@ -0,0 +1,8 @@
+public class Family {
+    public static void main(String[] args) {
+        System.out.println("Dima");
+        System.out.println("Danya");
+        System.out.println("Tanya");
+        System.out.println("Liza");
+    }
+}
